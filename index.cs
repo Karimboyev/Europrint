0415c0e72da4f5c80aef5eb9577d35a1228e53c4
007odilbek <007odilbek@gmail.com>
2021-02-28 09:51:35 +0500

    load

---
+++ index.cs
@@ -0,0 +1,259 @@
+* {
+  margin: 0;
+  padding: 0;
+}
+
+header {
+  position: relative;
+}
+header #bg-img {
+  position: absolute;
+  right: 200px;
+  top: -27px;
+  background-color: #E5E5E5;
+  max-width: 484.031px;
+  max-height: 460.719px;
+  width: 35.435vw;
+  height: 33.7287vw;
+  border-bottom-left-radius: 23.72vw;
+  z-index: -1;
+}
+header .navbar-collapse {
+  position: relative !important;
+}
+header .navbar {
+  margin-left: 0 !important;
+  padding-top: 27.2px !important;
+  background-color: rgba(255, 255, 255, 0) !important;
+}
+header .navbar .container {
+  padding-left: 1.26vw important;
+}
+header #brand-box {
+  width: 103px;
+  position: relative !important;
+}
+header #brand-box :nth-child(2) {
+  position: absolute !important;
+  left: 26.5px;
+  top: 11.17px;
+}
+header #brand-box :nth-child(3) {
+  position: absolute !important;
+  left: 13.58px;
+}
+header #brand-box :nth-child(4) {
+  position: absolute !important;
+  left: 57.12px;
+  top: 6.56px;
+}
+header #brand-box :nth-child(5) {
+  position: absolute !important;
+  left: 66.66px;
+  top: 6.56px;
+}
+header #brand-box :nth-child(6) {
+  position: absolute !important;
+  left: 79.36px;
+  top: 6.56px;
+}
+header #brand-box :nth-child(7) {
+  position: absolute !important;
+  left: 89.55px;
+  top: 6.56px;
+}
+header #brand-box :nth-child(8) {
+  position: absolute !important;
+  left: 57.12px;
+  top: 22.21px;
+}
+header #brand-box :nth-child(9) {
+  position: absolute !important;
+  left: 66.74px;
+  top: 22.21px;
+}
+header #brand-box :nth-child(10) {
+  position: absolute !important;
+  left: 77.19px;
+  top: 22.21px;
+}
+header #brand-box :nth-child(11) {
+  position: absolute !important;
+  left: 81.67px;
+  top: 22.21px;
+}
+header #brand-box :nth-child(12) {
+  position: absolute !important;
+  left: 93.32px;
+  top: 22.21px;
+}
+header .dropdown-menu {
+  min-width: 0 !important;
+}
+header .dropdown-menu li {
+  width: 100% !important;
+  margin-right: 0 !important;
+}
+header .dropdown-item:hover {
+  background-color: #ec681f !important;
+}
+header .dropdown-toggle {
+  border: 1px solid #cdcdcd !important;
+  border-radius: 100px !important;
+  padding: 6px 12px !important;
+  width: 120.266px !important;
+  height: 34.328px !important;
+  margin-top: 2.845px !important;
+}
+header .dropdown-toggle::after {
+  display: none !important;
+}
+header .navbar-nav {
+  margin-left: 323px !important;
+}
+header .navbar-nav li:nth-child(1) {
+  min-width: 116px !important;
+}
+header .navbar-nav li:nth-child(6) {
+  margin-right: 10px !important;
+}
+header .navbar-nav li {
+  margin-right: 15.179px !important;
+}
+header .navbar-nav li a {
+  position: relative !important;
+  color: #131a22 !important;
+  font-family: sans-serif !important;
+  font-style: normal !important;
+  font-weight: normal !important;
+}
+header .navbar-nav li a::after {
+  position: absolute;
+  content: "";
+  width: 90%;
+  left: 5%;
+  bottom: 7px;
+  border-bottom: 1px solid #F26522;
+  transform: scale(0);
+  transition: 0s;
+}
+header .navbar-nav li:hover a::after {
+  position: absolute;
+  content: "";
+  width: 90%;
+  left: 5%;
+  bottom: -3px;
+  border-bottom: 1px solid #F26522;
+  transform: scale(1);
+  transition: 0.35s;
+}
+header .navbar-nav :nth-child(7) {
+  margin-right: 0 !important;
+}
+
+@media (max-width: 1199px) and (min-width: 992px) {
+  header .navbar-nav {
+    margin-left: 140px !important;
+  }
+}
+@media (max-width: 992px) {
+  header .navbar-nav {
+    margin-left: 5px !important;
+  }
+  header .navbar-nav a::after {
+    display: none !important;
+  }
+  header .navbar-nav .dropdown-menu {
+    width: 125px !important;
+  }
+
+  main {
+    text-align: center !important;
+  }
+  main #corusel-box {
+    position: relative !important;
+  }
+}
+main .container {
+  margin-bottom: 190px;
+}
+main .container #section1 {
+  position: relative !important;
+}
+main .container #text-box {
+  padding-top: 120px;
+  width: 45.36111vw !important;
+  max-width: 682px !important;
+  font-size: 44px !important;
+  font-family: sans-serif !important;
+  color: #131A22 !important;
+}
+main .container #text-box div {
+  margin-top: 130px;
+  margin-right: 14px;
+}
+main .container #text-box div img:nth-child(2) {
+  padding-top: 34px;
+}
+main .container #text-box button {
+  background-color: #F26522 !important;
+  border-radius: 100px !important;
+}
+main .container #text-box p {
+  font-weight: 100 !important;
+}
+main .container #text-box span {
+  font-weight: 700 !important;
+}
+main .container #corusel-box {
+  max-width: 562.078px !important;
+  max-height: 536px !important;
+  background: #fff !important;
+  position: absolute;
+  background-color: rgba(255, 255, 255, 0) !important;
+  width: 42.7vw;
+  height: 39.733vw;
+  right: -100px !important;
+  top: 0;
+}
+main .container #corusel-box img {
+  width: 42.7vw;
+  height: 39.733vw;
+  max-width: 562.078px !important;
+  max-height: 536px !important;
+}
+main #section2 {
+  position: relative;
+  margin-bottom: 240px;
+}
+main #section2 div h1 {
+  max-width: 441px;
+  width: 30.625vw;
+  margin-bottom: 39px;
+}
+main #section2 div p {
+  max-width: 585px;
+  width: 40.625vw;
+}
+main #section2 #section2-img {
+  position: absolute;
+  right: -120px;
+  top: 0;
+}
+main #section2 #section2-img2 {
+  position: absolute;
+  right: 15px;
+  top: 295.51px;
+}
+main #section3 {
+  height: 750px;
+  position: relative !important;
+}
+main #section3 #section3-background {
+  position: absolute;
+  top: 0;
+  left: -10%;
+  max-width: 1300px !important;
+}
+
+/*# sourceMappingURL=index.cs.map */
